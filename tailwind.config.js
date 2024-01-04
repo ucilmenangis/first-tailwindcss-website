b4bc86fbc95081a6919da1fcc1ce7cab9d6ae054
Ucil Menangis <adik.irfan2006@gmail.com>
2024-01-04 22:15:35 +0700

    first website with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Test/**/*.{html,js}"],
+  theme: {
+    extend: {
+      
+    },
+    fontFamily: {
+      'roboto': ['Roboto'],
+    }
+  },
+  plugins: [],
+}
+
