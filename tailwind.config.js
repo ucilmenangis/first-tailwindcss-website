1cc6d442f67c981d12594c86e229cd514fa92523
Ucil Menangis <adik.irfan2006@gmail.com>
2024-01-06 23:06:23 +0700

    add some features

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,22 @@ module.exports = {
   content: ["./Test/**/*.{html,js}"],
   theme: {
     extend: {
+<<<<<<< HEAD
       
     },
     fontFamily: {
       'roboto': ['Roboto'],
+=======
+      backgroundImage: {
+        'img-tampilan' : "url(img/blue-gradient.avif)",
+      }
+    },
+    fontFamily: {
+      'roboto': ['Roboto'],
+    },
+    textShadow: {
+      'text-shadow' : '0 2px 4px var(--tw-shadow-color)',
+>>>>>>> 4582d98 (add some features)
     }
   },
   plugins: [],
