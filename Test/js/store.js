4531c42b27821956e168eb007728f489a84c0d66
Ucil Menangis <adik.irfan2006@gmail.com>
2024-01-09 01:23:14 +0700

    responsive item

---
+++ Test/js/store.js
@@ -0,0 +1,11 @@
+let navbarButton = document.getElementById('navbar-button')
+let navbarResponsiveUI = document.getElementById('nav-res')
+
+navbarButton.addEventListener('click',() => {
+    navbarResponsiveUI.classList.toggle('top-14')
+    navbarResponsiveUI.classList.toggle('-translate-y-48')
+    // navbarResponsiveUI.classList.toggle('top-14')
+    // navbarResponsiveUI.classList.toggle('inherit')
+    // navbarResponsiveUI.classList.toggle('hidden')
+    console.log('berhasil')
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,16 @@ module.exports = {
   content: ["./Test/**/*.{html,js}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'img-tampilan' : "url(img/blue-gradient.avif)",
-      }
+
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'md-780': '780px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      'sm-v': '550px',
     },
     fontFamily: {
       'roboto': ['Roboto'],
